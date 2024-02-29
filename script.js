cccd9760057bc65fb72b6f90c5ffbc92ad567109
LuzDelCarmenAguilarAngeles <l20200855@pachuca.tecnm.mx>
2024-02-29 10:25:29 -0600

    Version inicial

---
+++ script.js
@@ -0,0 +1,18 @@
+const startButton = document.getElementById('startButton');
+const resultDiv = document.getElementById('result');
+
+if ('webkitSpeechRecognition' in window) {
+    const recognition = new webkitSpeechRecognition();
+    recognition.lang = 'es-ES';
+
+    startButton.addEventListener('click', function() {
+        recognition.start();
+    });
+
+    recognition.onresult = function(event) {
+        const result = event.results[0][0].transcript;
+        resultDiv.innerHTML = `<p>Orden identificada: ${result}</p>`;
+    };
+} else {
+    resultDiv.innerHTML = "<p>La funcionalidad de reconocimiento de voz no está disponible en tu navegador.</p>";
+}
